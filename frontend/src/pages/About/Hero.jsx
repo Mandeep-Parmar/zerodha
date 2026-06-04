aec8d5ca96a581f05aec5499dd812f1f39940cd8
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-06-04 19:30:36 +0530

    implement about page

--- frontend/src/pages/About/Hero.jsx
+++ frontend/src/pages/About/Hero.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="container p-3">
+      <div className="row text-center py-5 border-bottom mx-lg-5">
+        <h1 className="my-5 fs-3">
+          We pioneered the discount broking model in India. <br />
+          Now, we are breaking ground with our technology.
+        </h1>
+      </div>
+      <div className="row align-items-center">
+        {/* Left Content */}
+        <div
+          className="col-lg-6 col-md-12 mb-4 mb-lg-0 px-lg-5 text-muted"
+          style={{ fontSize: "1.1rem" }}
+        >
+          <p className="mt-5 px-lg-5">
+            We kick-started operations on the 15th of August, 2010 with the goal
+            of breaking all barriers that traders and investors face in India in
+            terms of cost, support, and technology. We named the company
+            Zerodha, a combination of Zero and "Rodha", the Sanskrit word for
+            barrier.
+          </p>
 
-export default Hero
+          <p className="mt-4 px-lg-5">
+            Today, our disruptive pricing models and in-house technology have
+            made us the biggest stock broker in India.
+          </p>
+
+          <p className="mt-4 px-lg-5">
+            Over 1.6+ crore clients place billions of orders every year through
+            our powerful ecosystem of investment platforms, contributing over
+            15% of all Indian retail trading volumes.
+          </p>
+        </div>
+
+        {/* Right Content */}
+        <div
+          className="col-lg-6 col-md-12 mb-4 mb-lg-0 px-lg-5 text-muted"
+          style={{ fontSize: "1.1rem" }}
+        >
+          <p className="mt-4 px-lg-5">
+            In addition, we run a number of popular open online educational and
+            community initiatives to empower retail traders and investors.
+          </p>
+
+          <p className="mt-4 px-lg-5">
+            <a href="#" className="text-decoration-none">
+              Rainmatter
+            </a>
+            , our fintech fund and incubator, has invested in several fintech
+            startups with the goal of growing the Indian capital markets.
+          </p>
+
+          <p className="mt-4 px-lg-5">
+            And yet, we are always up to something new every day. Catch up on
+            the latest updates on our{" "}
+            <a href="#" className="text-decoration-none">
+              blog
+            </a>{" "}
+            or see what the media is{" "}
+            <a href="#" className="text-decoration-none">
+              saying about us
+            </a>{" "}
+            or learn more about our business and product{" "}
+            <a href="#" className="text-decoration-none">
+              philosophies
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- frontend/src/pages/About/Team.jsx
+++ frontend/src/pages/About/Team.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
 
 const Team = () => {
   return (
-    <div>Team</div>
-  )
-}
+    <div className="container p-3">
+      <div className="row text-center py-5 mx-lg-5">
+        <h1 className="mb-5 fs-3">People</h1>
+      </div>
 
-export default Team
+      <div className="row align-items-center">
+        {/* Left Image */}
+        <div className="col-lg-6 col-md-12 mb-4 mb-lg-0 text-muted text-center">
+          <img
+            src="/media/images/nithinKamath.jpg"
+            alt="Nithin Kamath"
+            className="img-fluid"
+            style={{ borderRadius: "100%", width: "45%" }}
+          />
+          <h5 className="mt-4">Nithin Kamath</h5>
+          <p className="text-muted mt-2">Founder, CEO</p>
+        </div>
+
+        {/* Right Content */}
+        <div
+          className="col-lg-6 col-md-12 mb-4 px-lg-5 text-muted"
+          style={{ fontSize: "1.1rem" }}
+        >
+          <p className="px-lg-5">
+            Nithin bootstrapped and founded Zerodha in 2010 to overcome the
+            hurdles he faced during his decade long stint as a trader. Today,
+            Zerodha has changed the landscape of the Indian broking industry
+          </p>
+
+          <p className="mt-4 px-lg-5">
+            He is a member of the SEBI Secondary Market Advisory Committee
+            (SMAC) and the Market Data Advisory Committee (MDAC).
+          </p>
+
+          <p className="mt-4 px-lg-5">Playing basketball is his zen.</p>
+
+          <p className="mt-4 px-lg-5">
+            Connect on{" "}
+            <a href="#" className="text-decoration-none">
+              Homepage
+            </a>{" "}
+            /{" "}
+            <a href="#" className="text-decoration-none">
+              TradingQnA
+            </a>{" "}
+            /{" "}
+            <a href="#" className="text-decoration-none">
+              Twitter
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Team;
