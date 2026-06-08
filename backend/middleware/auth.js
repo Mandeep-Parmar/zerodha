c4fc0b1fd62d134db70cd76b844ba4ee21f8a88c
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-06-08 18:04:00 +0530

    implement standard JWT auth middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+const authUser = async (req, res, next) => {
+  try {
+    const { token } = req.headers;
+
+    if (!token) {
+      return res.json({
+        success: false,
+        message: "Not Authorized Login Again",
+      });
+    }
+
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = token_decode; // contains user id
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "invalid token" });
+  }
+};
+
+export default authUser;
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import holdingRouter from "./routes/holdingRoute.js";
 import positionRouter from "./routes/positionRoute.js";
 import orderRouter from "./routes/OrderRoute.js";
 import userRouter from "./routes/userRoute.js";
+import authUser from "./middleware/auth.js";
 
 const app = express();
 connectDB();
@@ -17,9 +18,9 @@ app.use(cors());
 const port = process.env.PORT || 4000;
 
 // api routes
-app.use("/api/holdings", holdingRouter);
-app.use("/api/positions", positionRouter);
-app.use("/api/orders", orderRouter);
+app.use("/api/holdings", authUser, holdingRouter);
+app.use("/api/positions", authUser, positionRouter);
+app.use("/api/orders", authUser, orderRouter);
 app.use("/api/users", userRouter);
 
 app.get("/", (req, res) => {
