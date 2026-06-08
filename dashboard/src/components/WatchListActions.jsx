fd6e224ec0fc5046c673ee8c321b12415a6c4375
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-06-08 11:35:49 +0530

    remove deprecated TransitionComponent from MUI Tooltip

--- dashboard/src/components/WatchListActions.jsx
+++ dashboard/src/components/WatchListActions.jsx
@@ -11,43 +11,23 @@ const WatchListActions = ({ uid }) => {
   return (
     <span className="actions">
       <span>
-        <Tooltip
-          title="Buy (B)"
-          placement="top"
-          arrow
-          TransitionComponent={Grow}
-        >
+        <Tooltip title="Buy (B)" placement="top" arrow>
           <button className="buy" onClick={() => handleOpenBuyWindow(uid)}>
             Buy
           </button>
         </Tooltip>
 
-        <Tooltip
-          title="Sell (S)"
-          placement="top"
-          arrow
-          TransitionComponent={Grow}
-        >
+        <Tooltip title="Sell (S)" placement="top" arrow>
           <button className="sell">Sell</button>
         </Tooltip>
 
-        <Tooltip
-          title="Analytics (A)"
-          placement="top"
-          arrow
-          TransitionComponent={Grow}
-        >
+        <Tooltip title="Analytics (A)" placement="top" arrow>
           <button className="action">
             <BarChartOutlined className="icon" />
           </button>
         </Tooltip>
 
-        <Tooltip
-          title="More (M)"
-          placement="top"
-          arrow
-          TransitionComponent={Grow}
-        >
+        <Tooltip title="More (M)" placement="top" arrow>
           <button className="action">
             <MoreHoriz className="icon" />
           </button>
